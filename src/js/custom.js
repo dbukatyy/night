4b872ca42cf3af15ae90ce900bb75142ebf50667
dbukatyy <dbukatyy@mail.ru>
2017-04-05 15:28:11 +0300

    finish

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,8 +1,21 @@
+//= scroll/velocity.min.js
+//= scroll/velocity.ui.min.js
+//= scroll/main.js
+
 jQuery(document).ready(function () {
+
+    $(window).on('scroll', e => {
+
+        if ( $(this).scrollTop() > 150 ) {
+            $('.header').addClass('active');
+        } else {
+            $('.header').removeClass('active');
+        }
+    })
    
    $('.afisha-slider').slick({
  		infinite: false,
- 		centerMode: true,
+ 		// centerMode: true,
  		slidesToShow: 1
   	});
 
@@ -61,6 +74,71 @@ jQuery(document).ready(function () {
 	
 	$('.btn-close, .menu__link').on('click', e => {
 		$('.menu').removeClass('active');
+	});
+
+	 //ajax
+    $('.form .btn').on('click', function (e) {
+		e.preventDefault();
+
+		var msg = $('.form__msg'),
+            // message = $('.form-alert .msg'),
+            form = $(this).closest('.form'),
+            inputs =  form.find('.form__field'),
+            // errorMsg = form.find('.error'),
+            valid = validate();
+
+
+        function validate () {
+       
+            var valid = true;
+
+            // console.log(inputs );
+
+            inputs.each( function () {
+
+                if ( $(this).val() === '' ) {
+                     // console.log($(this).val());
+                    valid = false;
+                    return false;
+                } 
+            });
+               
+            return valid;
+        }
+
+        function showMessage(data) {
+            msg.html(data);
+            msg.addClass('active');
+        }
+
+        if (valid) {
+
+             $.ajax({    
+                url: form.attr('action'),
+                data: form.serialize(),
+                type: 'POST',
+                success: function(data){
+                    showMessage(data);
+                    msg.css('background', 'rgba(39,179,101,1)');
+                },
+                error: function(){
+                    showMessage('Ошибка отправки. Пожалуйста, повторите попытку.');
+                    msg.css('background', 'rgba(158,26,47,1)');
+                },
+                complete: function(){
+                    setTimeout(function () {
+                        msg.removeClass('active');
+                    }, 2000);
+                    form[0].reset();
+                }
+            });
+         } else {
+            showMessage('Пожалуйста, заполните все поля.')
+            msg.css('background', 'rgba(158,26,47,1)');
+            setTimeout( function () {
+                msg.removeClass('active');
+            }, 2000);
+         }
 	});
 });
 
